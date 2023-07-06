c3283477b33706d86349853a7d4294f7bd43ab94
Chirag <chiragxt1@hotmail.com>
2023-07-06 18:12:17 +0530

    Created validation token handler

--- mycontacts-backend/routes/userRoutes.js
+++ mycontacts-backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { registerUser, loginUser, currentUser } = require("../controllers/userController");
+const validateToken = require("../middleware/valildateTokenHandler");
 
 const router = express.Router();
 
-router.post("/register", registerUser).post("/login", loginUser).get("/current", currentUser);
+router.post("/register", registerUser).post("/login", loginUser).get("/current", validateToken, currentUser);
 
 module.exports = router;
